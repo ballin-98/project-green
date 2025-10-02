db33744d28ac0a767964c5f44bbf43ccc510afc1
ballin-98 <alexbali189@gmail.com>
2025-10-01 23:06:16 -0600

    added some navigation

--- src/app/stock/new/AddStockForm.tsx
+++ src/app/stock/new/AddStockForm.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Box, TextField, Button, Typography } from "@mui/material";
 import { addNewStock } from "@/app/lib/stockService";
 import { useRouter } from "next/navigation";
+import { ArrowLeft } from "@mui/icons-material";
 
 export default function StockForm() {
   const [stockName, setStockName] = useState("");
@@ -26,57 +27,67 @@ export default function StockForm() {
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 2,
-        width: 400,
-        padding: 2,
-        boxShadow: 3,
-        borderRadius: 2,
-        backgroundColor: "white",
-        marginTop: 4,
-      }}
-    >
-      <Typography variant="h6">New Stock Information</Typography>
+    <>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          width: 400,
+          padding: 2,
+          boxShadow: 3,
+          borderRadius: 2,
+          backgroundColor: "white",
+          marginTop: 4,
+        }}
+      >
+        <Typography variant="h6">New Stock Information</Typography>
 
-      <TextField
-        label="Stock Name"
-        value={stockName}
-        onChange={(e) => setStockName(e.target.value)}
-        required
-      />
+        <TextField
+          label="Stock Name"
+          value={stockName}
+          onChange={(e) => setStockName(e.target.value)}
+          required
+        />
 
-      <TextField
-        label="Wealth Simple ($)"
-        type="number"
-        value={wealthSimple}
-        onChange={(e) => setWealthSimple(e.target.value)}
-        required
-      />
+        <TextField
+          label="Wealth Simple ($)"
+          type="number"
+          value={wealthSimple}
+          onChange={(e) => setWealthSimple(e.target.value)}
+          required
+        />
 
-      <TextField
-        label="Quest Trade ($)"
-        type="number"
-        value={questTrade}
-        onChange={(e) => setQuestTrade(e.target.value)}
-        required
-      />
+        <TextField
+          label="Quest Trade ($)"
+          type="number"
+          value={questTrade}
+          onChange={(e) => setQuestTrade(e.target.value)}
+          required
+        />
 
-      <TextField
-        label="Dividend Frequency (per year)"
-        type="number"
-        value={dividendFrequency}
-        onChange={(e) => setDividendFrequency(e.target.value)}
-        required
-      />
+        <TextField
+          label="Dividend Frequency (per year)"
+          type="number"
+          value={dividendFrequency}
+          onChange={(e) => setDividendFrequency(e.target.value)}
+          required
+        />
 
-      <Button variant="contained" type="submit">
-        Add Stock
+        <Button variant="contained" type="submit">
+          Add Stock
+        </Button>
+      </Box>
+      <Button
+        variant="contained"
+        sx={{ padding: 1, mt: 2 }}
+        startIcon={<ArrowLeft />}
+        onClick={() => router.push("/")}
+      >
+        Back
       </Button>
-    </Box>
+    </>
   );
 }
--- src/app/trades/new/NewTradeForm.tsx
+++ src/app/trades/new/NewTradeForm.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Box, TextField, Button, Typography } from "@mui/material";
 import { addTrade } from "@/app/lib/stockService";
 import { useRouter } from "next/navigation";
+import { ArrowLeft } from "@mui/icons-material";
 
 export default function TradeForm() {
   const [stockName, setStockName] = useState("");
@@ -25,49 +26,59 @@ export default function TradeForm() {
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 2,
-        width: 400,
-        padding: 2,
-        boxShadow: 3,
-        borderRadius: 2,
-        backgroundColor: "white",
-        marginTop: 4,
-      }}
-    >
-      <Typography variant="h6">New Trade Information</Typography>
+    <>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          width: 400,
+          padding: 2,
+          boxShadow: 3,
+          borderRadius: 2,
+          backgroundColor: "white",
+          marginTop: 4,
+        }}
+      >
+        <Typography variant="h6">New Trade Information</Typography>
 
-      <TextField
-        label="Stock Name"
-        value={stockName}
-        onChange={(e) => setStockName(e.target.value)}
-        required
-      />
+        <TextField
+          label="Stock Name"
+          value={stockName}
+          onChange={(e) => setStockName(e.target.value)}
+          required
+        />
 
-      <TextField
-        label="Shares"
-        type="number"
-        value={shares}
-        onChange={(e) => setShares(e.target.value)}
-        required
-      />
+        <TextField
+          label="Shares"
+          type="number"
+          value={shares}
+          onChange={(e) => setShares(e.target.value)}
+          required
+        />
 
-      <TextField
-        label="Profit"
-        type="number"
-        value={profit}
-        onChange={(e) => setProfit(e.target.value)}
-        required
-      />
+        <TextField
+          label="Profit"
+          type="number"
+          value={profit}
+          onChange={(e) => setProfit(e.target.value)}
+          required
+        />
 
-      <Button variant="contained" type="submit">
-        Add Trade
+        <Button variant="contained" type="submit">
+          Add Trade
+        </Button>
+      </Box>
+      <Button
+        variant="contained"
+        sx={{ padding: 1, mt: 2 }}
+        startIcon={<ArrowLeft />}
+        onClick={() => router.push("/")}
+      >
+        Back
       </Button>
-    </Box>
+    </>
   );
 }
