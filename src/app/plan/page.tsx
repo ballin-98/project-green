1f6818fe252b8ea9fbf38464d534ff4a58133cf9
ballin-98 <alexbali189@gmail.com>
2025-10-27 23:39:45 -0600

    forcing component to be dynamic

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -1,6 +1,8 @@
 import { getStock } from "../lib/stockService";
 import PlanDashboard from "./PlanDashboard";
 
+export const dynamic = "force-dynamic";
+
 export default async function PlanPage() {
   const response = await getStock();
   const monthlyGoal = Number(process.env.NEXT_PUBLIC_MONTHLY_GOAL) || 0;
