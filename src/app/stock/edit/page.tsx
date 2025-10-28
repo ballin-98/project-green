30a0a4b595d6b1973aeabf9f05e4866c0990b170
ballin-98 <alexbali189@gmail.com>
2025-10-27 23:42:04 -0600

    everything dynamic

--- src/app/stock/edit/page.tsx
+++ src/app/stock/edit/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import EditStockForm from "./EditStockForm";
 
+export const dynamic = "force-dynamic";
+
 export default function NewStockPage() {
   <Suspense fallback={<div>Loading...</div>}>
     return <EditStockForm />;
--- src/app/stocks/page.tsx
+++ src/app/stocks/page.tsx
@@ -1,6 +1,8 @@
 import { getStock, getTrades } from "../lib/stockService";
 import Dashboard from "./dashboard";
 
+export const dynamic = "force-dynamic";
+
 export default async function StocksPage() {
   const response = await getStock();
   const trades = await getTrades();
