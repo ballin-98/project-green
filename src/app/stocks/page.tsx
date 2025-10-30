dae46a072019f2276c12e03ec43908b05fcdb725
ballin-98 <alexbali189@gmail.com>
2025-10-29 23:19:09 -0600

    fixed bug

--- src/app/stocks/page.tsx
+++ src/app/stocks/page.tsx
@@ -1,16 +1,8 @@
-// import { getStock, getTrades } from "../lib/stockService";
 import Dashboard from "./dashboard";
 
 export const dynamic = "force-dynamic";
 
 export default async function StocksPage() {
-  // const response = await getStock();
-  // const trades = await getTrades();
-  const monthlyGoal = Number(process.env.NEXT_PUBLIC_MONTHLY_GOAL) || 0;
-  const yearlyGoal = Number(process.env.NEXT_PUBLIC_YEARLY_GOAL) || 0;
-  console.log("Monthly Goal:", monthlyGoal);
-  console.log("Yearly Goal:", yearlyGoal);
-  // console.log("StocksPage response:", response);
   return (
     <div style={{ background: "#FAFAFA" }}>
       <Dashboard />
